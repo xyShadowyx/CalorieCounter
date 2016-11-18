854c46bf164034a1bb463e2338826577c2b1f088
Florian <Florian.Obladen@googlemail.com>
2016-11-18 10:25:42 +0100

    mein shit

--- app/src/main/java/de/fhdw/bfws115a/team1/caloriecounter/activities/grocerymanagement/ClickListener.java
+++ app/src/main/java/de/fhdw/bfws115a/team1/caloriecounter/activities/grocerymanagement/ClickListener.java
@@ -2,7 +2,6 @@ package de.fhdw.bfws115a.team1.caloriecounter.activities.grocerymanagement;
 
 import android.view.View;
 import de.fhdw.bfws115a.team1.caloriecounter.R;
-import de.fhdw.bfws115a.team1.caloriecounter.activities.quantityunitmanagement.*;
 
 /**
  * Created by Florian on 08.11.2016.
@@ -14,6 +13,8 @@ public class ClickListener implements View.OnClickListener {
     public ClickListener(de.fhdw.bfws115a.team1.caloriecounter.activities.quantityunitmanagement.ApplicationLogic applicationLogic) {
         mApplicationLogic = applicationLogic;
     }
+
+
 
     @Override
     public void onClick(View view) {
